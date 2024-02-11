1512a9cff88eea886397a648f385e53fa2279f48
jaider <jaiderlolxd@gail.com>
2024-02-11 13:01:19 -0500

    doc

--- backend/src/modules/users/dto/user-response.dto.ts
+++ backend/src/modules/users/dto/user-response.dto.ts
@@ -1,10 +1,13 @@
 import { User } from '../entities/user.entity'
 import { Res } from '../../../constants/response.interface'
+import { ApiProperty } from '@nestjs/swagger'
 
 export class UserResponseDto extends Res {
+  @ApiProperty({ type: User })
   data: User
 }
 
 export class UsersResponseDto extends Res {
+  @ApiProperty({ type: [User] })
   data: User[]
 }
